7ceda8b81c926d9d10affeda43ab12de85852251
Ben Hsieh <bqh5026@gmail.com>
2020-07-01 20:16:28 -0400

    Add navBar component and images to Home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Home />
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,46 @@
 import React from 'react';
+import mooncake from '../images/mooncake.jpg';
+import chocolateTriangle from '../images/Chocolate Triangle Cake.jpg';
+import coffeeTriangle from '../images/Coffee Triangle Cake.jpg';
+import taroRoll from '../images/Taro Roll.jpg';
 
 const Home = () => {
     return (
-        <header>
-        <div className="overlay"></div>
-        <nav>
-          <h2>Dashboard</h2>
-          <ul>
-            <li>
-              <a href="#">Stocks</a>
-            </li>
-            <li>
-              <a href="#">Crypto Currencies</a>
-            </li>
-            <li className="cart">
-              <a href="#">
-                <ion-icon name="cart-outline"></ion-icon>Portfolio<span>0</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    )
+      <div className="container">
+        <div className="image">
+          <img src={mooncake} alt="mooncake" />
+          <h3>Mooncake</h3>
+          <h3>$4.50</h3>
+          <a className="addToCart cart1" href="#">
+            Add to cart
+          </a>
+        </div>
+        <div className="image">
+          <img src={chocolateTriangle} alt="chocolate triangle cake" />
+          <h3>Chocolate Triangle Cake</h3>
+          <h3>$1.75</h3>
+          <a className="addToCart cart2" href="#">
+            Add to cart
+          </a>
+        </div>
+        <div className="image">
+          <img src={coffeeTriangle} alt="coffee triangle" />
+          <h3>Coffee Triangle</h3>
+          <h3>$1.75</h3>
+          <a className="addToCart cart3" href="#">
+            Add to cart
+          </a>
+        </div>
+        <div className="image">
+          <img src={taroRoll} alt="taro roll" />
+          <h3>Taro Roll</h3>
+          <h3>$1.75</h3>
+          <a className="addToCart cart4" href="#">
+            Add to cart
+          </a>
+        </div>
+      </div>
+    );
 }
 
 export default Home; 
